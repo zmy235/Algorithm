6ff4e68bc8ca0c262d87c324a683a2b1fb62e501
zmy235 <zmy235@qq.com>
2017-04-10 12:56:58 +0800

    快排比对

---
+++ QuickSort.cpp
@@ -0,0 +1,107 @@
+/*
+*快速排序实现及对比
+*
+*2017-4-10
+*
+*By veizi
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define SIZE 10000
+#define RANGE 10000
+
+void H(int& i,int& j)
+{
+    int t;
+    t=i,i=j,j=t;
+}
+
+int times=0;
+
+//枢轴可移动
+void qsort(int* a,int start,int end)
+{
+    times++;
+    int i = start;
+    int j = end;
+
+    while(i<j)
+    {
+        while( a[j]>=a[start] && j>i ) j--;
+        if( i<j )
+        {
+            a[i++]=a[j];
+            while( a[i]<=a[start] && i<j ) i++;
+            if( i<j ) a[j--]=a[i];
+        }
+    }
+    a[i] = a[start];
+    if( start<(i-1) ) qsort(a,start,i-1);
+    if( (j+1)<end ) qsort(a,j+1,end);
+}
+
+//枢轴不移动
+void Qsort(int* a,int start,int end)
+{
+    times++;
+    int i = start;
+    int j = end;
+    int t = start;
+
+    while(i<=j)
+    {
+        while( i<j && a[i]<=a[start] ) i++;
+
+        if( i==j && a[i]<=a[start])
+        {
+            t=i;
+            break;
+        }
+        else if(i==j && a[i]>a[start])
+        {
+            t=j-1;
+            break;
+        }
+        else if(i<j && a[i]>a[start])
+        {
+            t=i-1;
+            while( i<j && a[j]>=a[start] ) j--;
+            if(i<j) H(a[i++],a[j--]);
+            if(i>j)
+            {
+                t=i-1;
+                break;
+            }
+        }
+    }
+
+    H(a[start],a[t]);
+
+    if(t-1-start>1)Qsort(a,start,t-1);
+    if(end-t+1>1)Qsort(a,t+1,end);
+}
+
+
+int main()
+{
+    //随机数组生成
+    int w[SIZE];
+    for(int i=0; i<SIZE; i++)
+    w[i]=RANGE*rand()/(RAND_MAX + 1);
+
+    //未排序数组
+    for(int p=0; p<SIZE; p++) cout<<w[p]<<" ";
+    cout<<endl;
+
+    //进行排序
+    int* &&a(w);
+    Qsort(a,0,SIZE-1);
+    //qsort(a,0,SIZE-1);
+
+    //排序结果
+    for(int p=0; p<SIZE; p++) cout<<w[p]<<" ";
+    cout<<endl<<times;
+
+    return 0;
+}
